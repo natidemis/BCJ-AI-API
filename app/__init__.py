9b9a865779556d25b80b637239300a1a49eef7bd
Gitcelo <mfa5@hi.is>
2021-05-31 12:49:47 +0000

    vil pulla

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,16 @@
+"""
+@authors: Marcelo Audibert, Natanel Demissew
+May 2021
+
+API for AI web service
+"""
+
+"""
+TODO:
+
+Authentication, sanitization, valdiation
+"""
+
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import pandas as pandas
@@ -11,20 +24,16 @@ app.config.from_object('config')
 ai = ai()
 class Bug(Resource):
     def get(self):
-        print(Status.OK.value)
-        #print(request.json) #Sækja json ur requesti..
-        return {'ok': 'ok'},200
-
-
-    #if results:
-    #    return results,200
-    #else:
-    #    return {Lýsing á villu},404
+        req = request.json #Retrieve JSON from GET request
+        bugs = ai.get_similar_bugs_k(req['summary'], req['description'], req['k'])
+        if bugs[0].value == 200:
+            return bugs[1],bugs[0].value
+        return bugs[1],bugs[0].value
     
     def post(self):
         data = request.json
         print(data)
-        if ai.add
+        #if ai.add
         pass
     
     def patch(self):
