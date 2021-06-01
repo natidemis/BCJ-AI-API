ffd60c58558a5698d5dd76c3e06d95590bdc3c47
natidemis <natidemisket@gmail.com>
2021-06-01 11:01:34 +0000

    lagadi validate_date fallinu

--- helper.py
+++ helper.py
@@ -1,15 +1,16 @@
 """
 Helper classes for the app
 """
-import datetime
+from datetime import datetime
 class Validator:
 
     def __init__(self):
         """
         A class containing validation functions for the app
         """
-        def validate_datestring(self, stringdate):
-            try:
-                datetime.datetime.strptime(stringdate, '%Y-%m-%d')
-            except ValueError:
-                raise ValueError("Incorrect date")
+    def validate_datestring(self, stringdate):
+        try:
+            datetime.strptime(stringdate, '%Y-%m-%d')
+            return True
+        except:
+            return False
