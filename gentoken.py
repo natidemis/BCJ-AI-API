aabf21f47ed4254171afb2af8481e357bc4de28a
natidemis <natidemisket@gmail.com>
2021-06-03 13:07:07 +0000

    fjarlægði óþarfa línu

--- gentoken.py
+++ gentoken.py
@@ -1,7 +1,5 @@
 import secrets
-import os
 
-filepath = os.getcwd() + '/config.py'
 with open('config.py', 'w') as f:
     f.write('DEBUG = True \nSECRET_TOKEN = "{}"'.format(secrets.token_urlsafe(100)))
 
