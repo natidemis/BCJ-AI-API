be54b4cb1a5094e51cfbaeb7f86e6696379f61bb
unknown <natidemisket@gmail.com>
2021-06-04 13:12:17 +0000

    faerði gagnagrunn hlutanum i db.py

--- helper.py
+++ helper.py
@@ -15,10 +15,7 @@ from schema import Schema, And, Use, Optional, SchemaError,Or
 from enum import Enum
 from config import SECRET_TOKEN
 from flask import jsonify, make_response
-from dotenv import load_dotenv
-import psycopg2
-import os
-import dotenv
+
 
 class Message(Enum):
     VALID_INPUT = 'Valid input, check status for result'
@@ -75,34 +72,3 @@ class Helper:
     def auth_token(self,token):
         return token == SECRET_TOKEN
 
-
-class Database_setup:
-    load_dotenv()
-    CREATED = False
-    HOST = os.getenv('DB_HOST')
-    NAME = os.getenv('DB_NAME')
-    USER = os.getenv('DB_USER')
-    PASSWORD = os.getenv('DB_PASSWORD')
-    def __init__(self):
-        """
-        Class to setup the database table when required.
-        """
-    
-    def make_table(self, size: int):
-        """
-        One time use to set up the Vectors table, 
-        required to determine vector length.
-        """
-        if self.CREATED:
-            return
-        sql_file = open('sql/schema.sql','r')
-        schema = sql_file.read()
-        sql_file.close()
-        conn = psycopg2.connect(dbname=self.NAME,user=self.USER, password=self.PASSWORD, host=self.HOST)
-        cur = conn.cursor()
-        cur.execute(schema,[size])
-        conn.commit()
-        cur.close()
-        
-        conn.close()
-        self.CREATED = True
