ef44252ed76de505426dc42c1bedc434682fe159
unknown <natidemisket@gmail.com>
2021-07-06 09:59:36 +0000

    script for fetching vectors done.

--- fetch_vectors.py
+++ fetch_vectors.py
@@ -1,17 +1,32 @@
 import gdown
-import tarfile
 import os.path
+import logging
 
-output = 'GoogleNews-vectors-negative300.bin.gz' #name of file
-if not os.path.exists(output):
-    print("hmm")
-    url = 'https://drive.google.com/uc?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM' #sharable drive link
-    gdown.download(url, output, quiet=False)
+logging.getLogger().setLevel(logging.INFO)
 
-    #Ef við zippum sem tar skrá -- 
-    #file = tarfile.open(output)
-    #  
-    ## extracting file
-    #file.extractall('GoogleNews-vectors-negative300.bin')
-    #  
-    #file.close()
+npyvector = 'wordvectors.wv.vectors.npy' 
+
+gensimvector = 'wordvectors.wv'
+
+logging.info('Looking for vectors..')
+
+if not os.path.exists(npyvector):
+    try:
+        logging.info('Fetching numpy vectors')
+        npy_url = 'https://drive.google.com/uc?id=1Izx4in1jUvI1-tCV5S-brZrse_HRwgVj' #sharable drive link
+        gdown.download(npy_url, npyvector, quiet=False)
+    except:
+        logging.error("Failed to fetch numpy vectors.")
+else:
+    logging.info("Numpy vectors already in path")
+if not os.path.exists(gensimvector):
+    try:
+        logging.info('Fetching gensim vectors')
+        gensim_url = 'https://drive.google.com/uc?id=1QMwcpvnSMFq_VikgzPzK0QcZ6kqmXy2p' #sharable drive link
+        gdown.download(gensim_url,gensimvector, quiet=False)
+    except:
+        logging.error("Fetching gensim vectors failed.")
+else:
+    logging.info("Gensim vectors already in path.")
+
+
