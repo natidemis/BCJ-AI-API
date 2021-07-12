44f9bea57be49e0dcacbb1090ab08dee71f2a088
natidemis <natidemisket@gmail.com>
2021-07-12 11:15:11 +0000

    lintering og villu meðhöndlun

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+# pylint: disable=R0201
 """
 @authors: Gitcelo, natidemis
 May-June 2021
@@ -5,17 +6,17 @@ May-June 2021
 API for AI web service
 """
 
+import json
+import os
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api, reqparse
 from flask_httpauth import HTTPTokenAuth
-from bcj_ai import BCJAIapi as ai, BCJStatus 
 from schema import Schema, And, Or, Use, Optional, SchemaError
-from helper import Validator, Message
-import json
 import bleach
-import os
 import dotenv
 from dotenv import load_dotenv
+from bcj_ai import BCJAIapi as ai, BCJStatus
+from helper import Validator, Message
 
 load_dotenv()
 SECRET_TOKEN = os.getenv('SECRET_TOKEN')
@@ -27,9 +28,13 @@ auth = HTTPTokenAuth(scheme="Bearer")
 
 @auth.verify_token
 def verify_token(token):
+    """
+    Authentication method for the enviroment using this service
+    via SECRET_TOKEN
+    """
     if token==SECRET_TOKEN:
         return token
-    
+    return None
 class Bug(Resource):
     """
     Web service class for working with a usability problem (UP)
@@ -38,7 +43,7 @@ class Bug(Resource):
     def get(self):
         """
         GET method that fetches the k UPs that are most similar to the UP
-        
+
         Returns
         -------
         ID's of the k most similar UPs if everything went well, else an error message
@@ -48,8 +53,8 @@ class Bug(Resource):
         try:
             validator.validate_data_get(req) #Validate the JSON
         except(SchemaError, ValueError):
-            return make_response(jsonify({"message": Message.FAILURE.value}), 400) #Failure message if JSON is invalid
-        
+            return make_response(jsonify({"message": Message.FAILURE.value}), 400)
+
         summary = bleach.clean(req['summary'])
         description = bleach.clean(req['description'])
         if summary == "" and description == "":
@@ -61,12 +66,13 @@ class Bug(Resource):
                                      structured_info,
                                      k)
         return make_response(jsonify(data=bugs[1]),bugs[0].value)
-    
+
     @auth.login_required
     def post(self):
         """
-        Method for handling POST request on '/bug' used for inserting a UP to the AI and its database.
-        
+        Method for handling POST request on '/bug',
+        used for inserting a UP to the AI and its database.
+
         Returns
         -------
         A message with a brief description explaining the result for the request and status code.
@@ -77,14 +83,14 @@ class Bug(Resource):
             validator.validate_data(req)
         except(SchemaError, ValueError):
             return make_response(jsonify({'message': Message.FAILURE.value}),400)
- 
+
         if len(req['summary']) > 0 or len(req['description'])>0:
             return make_response(jsonify(data={'message': Message.VALID_INPUT.value}), ai.add_bug(
                 summary=bleach.clean(req['summary']),
                 description=bleach.clean(req['description']),
                 structured_info=req['structured_info']).value)
         return make_response(jsonify(data={'message': Message.UNFULFILLED_REQ.value}),400)
-    
+
     @auth.login_required
     def patch(self):
         """
@@ -100,13 +106,13 @@ class Bug(Resource):
             validator.validate_update_data(req)
         except(SchemaError, ValueError):
             return make_response(jsonify(data={'message': Message.FAILURE.value}), 400)
-        
+
         summary = bleach.clean(req['summary']) if 'summary' in req else None
         description = bleach.clean(req['description']) if 'description' in req else None
         return make_response(jsonify({'message': Message.VALID_INPUT.value}), ai.update_bug(
             summary=summary,
             description = description,
-            structured_info = req['structured_info']).value)  
+            structured_info = req['structured_info']).value)
 
     @auth.login_required
     def delete(self):
@@ -131,24 +137,7 @@ class Batch(Resource):
     """
     Web service class for working with a batch of bugs
     """
-    @auth.login_required
-    def get(self):
-        """
-        Method for handling a get http request on /batch for fetching a batch of UPs.
 
-        Returns
-        -------
-        Batch of Ups if the batch exists in the database, otherwise a message and status code
-        """
-        req = request.json
-        try:
-            validator.validate_id(req)
-        except(SchemaError, ValueError):
-            return make_response(jsonify({'message': Message.FAILURE.value}),400)
-        
-        batch = ai.get_batch_by_id(req['id'])
-        return make_response(jsonify(data=batch[1]), batch[0].value)
-    
     @auth.login_required
     def delete(self):
         """
@@ -163,14 +152,22 @@ class Batch(Resource):
             validator.validate_batch_id(req)
         except(SchemaError, ValueError):
             return {'message': Message.FAILURE.value},400
-       
+
         result = ai.remove_batch(req['batch_id'])
         if result == BCJStatus.OK:
             return make_response(jsonify({'message': Message.REMOVED.value}), result.value)
         return make_response(jsonify({'message': Message.INVALID.value}), result.value)
-    
+
     @auth.login_required
     def post(self):
+        """
+        Method for handling a post request on '/batch'.
+        Used for inserting multiple examples at once.
+
+        Returns
+        -------
+        Message with brief explanation and status code
+        """
         req = request.json
         data = []
         try:
@@ -194,7 +191,9 @@ class Batch(Resource):
                     raise ValueError('Both summary and description may not have string length of 0')
         except(SchemaError, ValueError):
             return make_response(jsonify({'message': Message.FAILURE.value}),400)
-        return make_response(jsonify(data={'message': Message.VALID_INPUT.value}), ai.add_batch(data).value)
-        
+        return make_response(jsonify(
+                                    data={'message': Message.VALID_INPUT.value}),
+                                    ai.add_batch(data).value)
+
 api.add_resource(Bug,'/bug')
 api.add_resource(Batch, '/batch')
