c846fe4139b2c8d375bb6983b57546c86436cc74
unknown <natidemisket@gmail.com>
2021-07-24 13:40:59 +0000

    fleiri tests

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ File that executes the app and sets up the database.
 #Set up the database tables
 import fetch_vectors #pylint: disable=W0611
 from log import logger
-logger.info('Starting server..')
 from db import Database #pylint: disable=C0413
+logger.info('Starting server..')
 db = Database()
 db.drop_table()
 table_created = db.make_table()
@@ -16,14 +16,27 @@ table_created = db.make_table()
 #Run app
 from app import app # pylint: disable=wrong-import-position
 
-if __name__ == '__main__':
+def main():
+    """
+    Starts app for development.
+    """
     if not table_created:
-        logger.error("Setting up the database failed, app will not run in development")
-    else:
         logger.info("Database initialized successfully, starting app in development..")
         app.run()
-elif table_created:
-    logger.info('Starting gunicorn app..')
-    gunicorn_app = app()
+    else:
+        logger.error("Setting up the database failed, app will not run in development")
+
+def gunicorn():
+    """
+    Method called when starting app in production.
+    """
+    if table_created:
+        logger.info('Starting gunicorn app..')
+        gunicorn_app = app()
+    else:
+        logger.error('Failed to start gunicorn app..')
+
+if __name__ == '__main__':
+    main()
 else:
-    logger.error('Failed to start gunicorn app..')
+    gunicorn()
