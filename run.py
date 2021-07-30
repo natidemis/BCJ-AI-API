db173b4b4a6b8defb86c4f243cbca05ff2102445
unknown <natidemisket@gmail.com>
2021-07-30 10:29:40 +0000

    nest asyncio

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ May 2021
 File that executes the app and sets up the database.
 """
 #Set up the database tables
-import fetch_vectors #pylint: disable=W0611
+
 from log import logger
 from db import Database #pylint: disable=C0413
 logger.info('Starting server..')
